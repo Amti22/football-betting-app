8268d0a6b11a3358d19aeb531d710bcdddd8dce7
Amti22 <43556378+Amti22@users.noreply.github.com>
2024-12-27 08:25:10 +0000

    Update Step2-Ui.py

--- Step2-Ui.py
+++ Step2-Ui.py
@@ -79,19 +79,81 @@ def read_matches(sport=None, date=None):
             'home_odds': row[15],  # Home team win odds at index 17 ('1X2 1')
             'draw_odds': row[16],  # Draw odds at index 18 ('1X2 X')
             'away_odds': row[17],  # Away team win odds at index 19 ('1X2 2')
-            'bts_yes': row[8],  # BTS Yes odds at index 7
-            'bts_no': row[9],  # BTS No odds at index 8
-            'dc_1x': row[10],  # Double Chance Home/Draw at index 9 ('DC 1X')
-            'dc_12': row[11],  # Double Chance Home/Away at index 10 ('DC 12')
-            'dc_x2': row[12],  # Double Chance Draw/Away at index 11 ('DC X2')
-            'dnb_1': row[13],  # Draw No Bet Home at index 12 ('DNB 1')
-            'dnb_2': row[14],  # Draw No Bet Away at index 13 ('DNB 2')
-            'ov_1d5': row[46],  # Double Chance Home/Draw at index 9 ('DC 1X')
-            'ov_2d5': row[48],  # Double Chance Home/Away at index 10 ('DC 12')
-            'ov_3d5': row[50],  # Double Chance Draw/Away at index 11 ('DC X2')
-            'un_1d5': row[47],  # Double Chance Home/Draw at index 9 ('DC 1X')
-            'un_2d5': row[49],  # Double Chance Home/Away at index 10 ('DC 12')
-            'un_3d5': row[51],  # Double Chance Draw/Away at index 11 ('DC X2')
+            'bts_yes': row[8],
+            'bts_no': row[9],
+            'dc_1x': row[10],
+            'dc_12': row[11],
+            'dc_x2': row[12],
+            'dnb_1': row[13],
+            'dnb_2': row[14],
+            'ov_1d5': row[78],
+            'ov_2d5': row[80],
+            'ov_3d5': row[82],
+            'un_1d5': row[79],
+            'un_2d5': row[81],
+            'un_3d5': row[83],
+
+            'ah1_m3d75': row[18],  # AH 1 -3.75
+            'ah2_m3d75': row[19],  # AH 2 -3.75
+            'ah1_m3d5': row[20],   # AH 1 -3.5
+            'ah2_m3d5': row[21],   # AH 2 -3.5
+            'ah1_m3d25': row[22],  # AH 1 -3.25
+            'ah2_m3d25': row[23],  # AH 2 -3.25
+            'ah1_m3': row[24],     # AH 1 -3.0
+            'ah2_m3': row[25],     # AH 2 -3.0
+
+            'ah1_m2d75': row[26],  # AH 1 -2.75
+            'ah2_m2d75': row[27],  # AH 2 -2.75
+            'ah1_m2d5': row[28],   # AH 1 -2.5
+            'ah2_m2d5': row[29],   # AH 2 -2.5
+            'ah1_m2d25': row[30],  # AH 1 -2.25
+            'ah2_m2d25': row[31],  # AH 2 -2.25
+            'ah1_m2': row[32],     # AH 1 -2.0
+            'ah2_m2': row[33],     # AH 2 -2.0
+
+            'ah1_m1d75': row[34],  # AH 1 -1.75
+            'ah2_m1d75': row[35],  # AH 2 -1.75
+            'ah1_m1d5': row[36],   # AH 1 -1.5
+            'ah2_m1d5': row[37],   # AH 2 -1.5
+            'ah1_m1d25': row[38],  # AH 1 -1.25
+            'ah2_m1d25': row[39],  # AH 2 -1.25
+            'ah1_m1': row[40],     # AH 1 -1.0
+            'ah2_m1': row[41],     # AH 2 -1.0
+
+            'ah1_m0d75': row[42],  # AH 1 -0.75
+            'ah2_m0d75': row[43],  # AH 2 -0.75
+            'ah1_m0d5': row[44],   # AH 1 -0.5
+            'ah2_m0d5': row[45],   # AH 2 -0.5
+            'ah1_m0d25': row[46],  # AH 1 -0.25
+            'ah2_m0d25': row[47],  # AH 2 -0.25
+            'ah1_m0': row[48],     # AH 1 0.0
+            'ah2_m0': row[49],     # AH 2 0.0
+
+            'ah1_p1d75': row[50],  # AH 1 +0.75
+            'ah2_p1d75': row[51],  # AH 2 +0.75
+            'ah1_p1d5': row[52],   # AH 1 +1.5
+            'ah2_p1d5': row[53],   # AH 2 +1.5
+            'ah1_p1d25': row[54],  # AH 1 +1.25
+            'ah2_p1d25': row[55],  # AH 2 +1.25
+            'ah1_p1': row[56],     # AH 1 +1.0
+            'ah2_p1': row[57],     # AH 2 +1.0
+
+            'ah1_p2d75': row[58],  # AH 1 +2.75
+            'ah2_p2d75': row[59],  # AH 2 +2.75
+            'ah1_p2d5': row[60],   # AH 1 +2.5
+            'ah2_p2d5': row[61],   # AH 2 +2.5
+            'ah1_p2d25': row[62],  # AH 1 +2.25
+            'ah2_p2d25': row[63],  # AH 2 +2.25
+            'ah1_p2': row[64],     # AH 1 +2.0
+            'ah2_p2': row[65],     # AH 2 +2.0
+
+            'ah1_p3d5': row[76],   # AH 1 +3.5
+            'ah2_p3d5': row[77],   # AH 2 +3.5
+            'ah1_p3d25': row[74],  # AH 1 +3.25
+            'ah2_p3d25': row[75],  # AH 2 +3.25
+            'ah1_p3': row[72],     # AH 1 +3.0
+            'ah2_p3': row[73]      # AH 2 +3.0
+
         }
 
     return matches
